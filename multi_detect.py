019c67706de47edf3785d224aa2b3fb10b230e3c
wanbiguizhao <shiweishiweihubugui@outlook.com>
2018-11-24 15:13:35 +0800

    测试使用真实的mp3数据

--- multi_detect.py
+++ multi_detect.py
@@ -3,22 +3,24 @@
 from __future__ import print_function
 import speech_segmentation as seg
 
-frame_size = 256
-frame_shift = 128
-sr = 16000
+frame_size = 512
+frame_shift = 256
+sr = None
 
-seg_point = seg.multi_segmentation("duihua_sample.wav", sr, frame_size, frame_shift, plot_seg=True, save_seg=True,
+seg_point = seg.multi_segmentation("tingvoa.com_02.mp3", sr, frame_size, frame_shift, plot_seg=True, save_seg=True,
                                    cluster_method='bic')
 print('The segmentation point for this audio file is listed (Unit: /s)',seg_point)
 
 
 from pydub import AudioSegment
-song=AudioSegment.from_wav("duihua_sample.wav")
+song=AudioSegment.from_mp3("tingvoa.com_02.mp3")
+i=1
 last_point=0
 for x in seg_point:
     segment=song[last_point:x*1000]
     last_point=x*1000
-    segment.export(str(x*1000)+".wav",format="wav")
+    segment.export("save_audio/"+str(i)+"_"+str(x*1000)+".mp3",format="mp3")
+    i=i+1
 
 
 
