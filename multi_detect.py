5424e1f8436e411e474b8803a0d2e67302b64907
wanbiguizhao <shiweishiweihubugui@outlook.com>
2018-11-24 16:50:02 +0800

    修正变量名

--- multi_detect.py
+++ multi_detect.py
@@ -18,8 +18,8 @@ i=1
 beg_point=0
 for x in seg_point:
     end_point=x*1000
-    segment=song[last_point:end_point]
-    segment.export("save_audio/{:0>2d}_{:0>7d}_{:0>7d}_".format(i,beg_point,last_point)+".mp3",format="mp3")
+    segment=song[beg_point:end_point]
+    segment.export("save_audio/{:0>2d}_{:0>7d}_{:0>7d}_".format(i,beg_point,end_point)+".mp3",format="mp3")
     beg_point=end_point
     i=i+1
 segment=song[end_point:]
